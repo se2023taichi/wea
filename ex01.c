b4500c04533d94def944f5a8206c7a47a43c70c0
se2023taichi <r202203462pl@jindai.jp>
2023-12-18 14:47:15 +0900

    hairetu hello world

--- ex01.c
+++ ex01.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 int main(void) {
-  int i, n;
+  int i;
   char are[11] = "hello world";
   for (i = 0; i < 12; i++) {
     printf("%c", are[i]);
   }
-  printf("\n");
-  printf("%d\n", i);
   return 0;
 }
